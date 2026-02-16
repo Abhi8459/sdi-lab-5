f4c10908819bcbae3e0b1f0ce78d08b7660d93c7
Abhi8459 <abhishekyada9v@gmail.com>
2026-02-16 10:42:01 +0530

    file chnaged

--- BasicOfJava.java
+++ BasicOfJava.java
@@ -10,5 +10,6 @@ public class BasicOfJava{
     public static void main(String args[]){
      System.out.println(getIthBit(10,2));
      System.out.println("Abhishek Yadav");
+     System.out.println("Root ");
     }
 }
